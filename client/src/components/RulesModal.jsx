4f8e5f349cb3798b81f4a87542917e6dd6c300b8
Amit Jhamb <amit.jhamb@outlook.com>
2026-02-06 16:55:45 -0600

    Help button on landing page

--- client/src/components/RulesModal.jsx
+++ client/src/components/RulesModal.jsx
@@ -29,7 +29,7 @@ export default function RulesModal({ config, onClose }) {
                 <ul style={{ textAlign: 'left', marginBottom: '2rem', paddingLeft: '1.5rem', lineHeight: '1.8rem' }}>
                     <li><strong>Total Budget:</strong> {config.budget} Th</li>
                     <li><strong>Base Price:</strong> {config.basePrice} Th</li>
-                    <li><strong>Max Squad Size:</strong> {config.playersPerTeam} Players</li>
+                    <li><strong>Max Squad Size(Excluding Captain):</strong> {config.playersPerTeam} Players</li>
                     <li><strong>Max Bid Limit:</strong> {config.maxBid ? `${config.maxBid} Th` : 'Unlimited'}</li>
                     <li><strong>Min Reserve:</strong> You must keep enough budget to fill your squad at Base Price.</li>
                 </ul>
